9eb41dd0d07762e5499648a59973b7aa75679510
tomashevia27 <tomashevia27@gmail.com>
2023-11-15 22:40:35 -0300

    entrega

--- README.md
+++ README.md
@@ -9,52 +9,96 @@
 - Para compilar:
 
 ```bash
-línea de compilación
+make pruebas_chanutron
 ```
 
 - Para ejecutar:
 
 ```bash
-línea de ejecución
+./pruebas_chanutron
 ```
 
 - Para ejecutar con valgrind:
 ```bash
-línea con valgrind
+make valgrind-chanutron
 ```
 ---
 ##  Funcionamiento
+En este tda se desarrollaron las estructuras y operaciones para implementar un hash.
 
-Explicación de cómo funcionan las estructuras desarrolladas en el TP y el funcionamiento general del mismo.
-
-Aclarar en esta parte todas las decisiones que se tomaron al realizar el TP, cosas que no se aclaren en el enunciado, fragmentos de código que necesiten explicación extra, etc.
-
-Incluír **EN TODOS LOS TPS** los diagramas relevantes al problema (mayormente diagramas de memoria para explicar las estructuras, pero se pueden utilizar otros diagramas si es necesario).
-
-### Por ejemplo:
-
-El programa funciona abriendo el archivo pasado como parámetro y leyendolo línea por línea. Por cada línea crea un registro e intenta agregarlo al vector. La función de lectura intenta leer todo el archivo o hasta encontrar el primer error. Devuelve un vector con todos los registros creados.
-
-<div align="center">
-<img width="70%" src="img/diagrama1.svg">
-</div>
-
-En el archivo `sarasa.c` la función `funcion1` utiliza `realloc` para agrandar la zona de memoria utilizada para conquistar el mundo. El resultado de `realloc` lo guardo en una variable auxiliar para no perder el puntero original en caso de error:
-
+las estructuras utilizadas fueron:
 ```c
-int *vector = realloc(vector_original, (n+1)*sizeof(int));
+typedef struct par{
+	void *valor;
+	char *clave;
+	struct par* siguiente;
+}par_t;
 
-if(vector == NULL)
-    return -1;
-vector_original = vector;
+struct hash{
+	par_t **tabla_hash;
+	size_t capacidad;
+	size_t cantidad;
+};
 ```
 
+paso a explicar las funciones implementadas:
 
+`hash_crear`: en esta funcion se reserva la memoria necesaria para crear el hash, asignandole la capacidad recibida por parametro (minimo 3 sera la capacidad) y asignando memoria para la tabla hash.
+
+`hash_insertar`: se recibe un hash, una clave, un elemento y un puntero a void* anterior.
+esta funcion busca insertar el par clave-elemento dentro del hash.
+para esto, se calcula la posicion en la que se insertara el par mediante la `funcion_hash` (uso la djb2).
+luego de tener la posicion donde se tiene que insertar, se verifica si esa clave ya se encuentra en esa posicion, si se encuentra se reemplaza el valor, se guarda en anterior el valor que estaba antes y se devuelve el hash.
+si la clave no se encuentra en el hash, se crea un nuevo par mediante la funcion `crear_par_nuevo`, el par nuevo contiene una copia de la clave recibida por parametro y el elemento recibido. luego se inserta el nuevo par en la posicion designada (el nuevo par se ubica en el primer elemento de los pares encadenados), se aumenta la cantidad de elementos del hash y se retorna el mismo.
+al inicio de esta funcion, se calcula el factor de carga del hash, si este supera el 0.7%, se invoca a la funcion `rehashear`, esta funcion se encarga de crear un nuevo hash con el doble de capacidad que el anterior, e insertar los pares en este nuevo hash, luego el puntero a hash recibido apunta al hash nuevo y se libera la memoria anteriormente usada.
+
+`hash_quitar`: esta funcion recibe un hash y una clave la cual se quiere eliminar del hash.
+lo que se hace es, mediante `funcion_hash`, calcular la posicion en la que se deberia encontrar el elemento buscado en la tabla de hash, luego se recorren los pares enlazados en esa posicion, y si se encuentra el par buscado, se lo elimina liberando su memoria y se disminuye la cantidad de elementos del hash, si el elemento no se encuentra luego de recorrer todos los pares, la funcion retornara NULL.
+
+las funciones `hash_obtener` y `hash_contiene` funcionan de la misma manera, con la diferencia de que el par no se elimina del hash y se devuelve el elemento encontrado o NULL en `hash_obtener` y true o false en `hash_contiene`
+
+`hash_cantidad`: devuelve la cantidad de elementos del hash
+
+`hash_destruir_todo`: esta funcion se encarga de liberar toda la memoria del hash. se va recorriendo todo el hash mientras se liberan las claves y los pares, y aparte se destruyen los elementos con la funcion destructora que se recibe por parametro, luego de recorrer todos los pares, se libera la tabla de hash y el hash en si.
+
+`hash_destruir`: se utiliza la funcion `hash_destruir_todo` pero se envia una funcion nula para que no se destruyan los elementos. libera toda la memoria del hash.
+
+`hash_con_cada_clave`: esta funcion itera y recorre todos los pares del hash y les aplica la funcion booleana que recibe por parametro, si la funcion devuelve false, se corta la iteracion.
+termina retornando la cantidad de pares a los que se les aplico la funcion booleana.
+
+DIAGRAMA DE MEMORIA DE HASH:
 <div align="center">
-<img width="70%" src="img/diagrama2.svg">
+<img width="70%" src="img/diagrama_hash.png">
 </div>
 
 ---
 
 ## Respuestas a las preguntas teóricas
-Incluír acá las respuestas a las preguntas del enunciado (si aplica).
+
+PREGUNTA 1: Que es un diccionario?
+Un diccionario es un tipo de dato que, al igual que las listas, arboles y demas tdas que vimos, se utiliza para almacenar datos.
+El beneficio, y lo que se busca al implementar este tipo de dato, es su rapidez en la busqueda de los elementos.
+Ya que se tiene acceso inmediato a las direcciones de memoria almacenadas en el diccionario.
+Los valores se guardan en pares clave-valor, donde la clave es importante para asignarle lugar al par dentro del diccionario, y mediante esa clave se realiza la busqueda del valor en el diccionario.
+
+PREGUNTA 2: Qué es una función de hash y qué características debe tener?
+La funcion de hash es una funcion que transforma la clave recibida en un valor numerico, el cual se utiliza para asignar la posicion del apr clave-valor en el diccionario. Las caracteristicas que debe tener es que el resultado debe ser mayor que 0 y menor que la capacidad que tiene el diccionario para almacenar elementos, ademas siempre que recibe una clave, esa clave debe retornar el mismo valor numerico.
+
+PREGUNTA 3: Qué es una tabla de Hash y los diferentes métodos de resolución de colisiones vistos (encadenamiento, probing, zona de desborde)?
+Una tabla de hash es una estructura que contiene valores (pares clave-valor), estos valores tienen una posicion asignada en la tabla de hash. la tabla guarda los valores junto con una clave, esta clave (a traves de la funcion de hash) sirve para asignar la posicion dentro de la tabla.
+La tabla de hash (junto con la funcion hash) permite guardar los valores y acceder a ellos muy facilmente, ya que sabemos la posicion en la que este valor se encuentra, esa es la gran ventaja de los diccionarios.
+Puede suceder que varias claves caigan en la misma posicion de la tabla, a esto se lo llama colisiones. Las colisiones se pueden resolver de diferentes formas:
+
+ - Encadenamiento: es la que usamos en esta implementacion. se basa en usar nodos enlazados (o listas, o abb o cualquier estructura) para almacenar los datos que coinciden en esa posicion de la tabla. Dentro de una posicion de la tabla, se encuentran varios pares claves-valor. Osea, cuando se le aplica la funcion hash a una clave, y en la posicion en la que "cae" el par ya hay uno o mas pares asignados, se "encadena" el nuevo par a los pares ya almacenados en esa posicion. Aca la busqueda no va a ser al 100% O(1), ya que puede que tengamos que recorrer algunos elementos hasta encontrarlos, pero aun asi la busqueda sigue siendo muy eficiente.
+
+ - Probing: si al aplicar la funcion hash, el par "cae" en una posicion ya ocupada, lo que se hace es recorrer la tabla de hash hasta encontrar la siguiente ubicacion vacia y ahi se guarda el nuevo par (se lo llama probing lineal), tambien se podria usa un probing cuadratico o usando una segunda funcion de hash buscando otra ubicacion.
+
+ - Zona de desborde: cuando la posicion esta ocupada, el elemento va a la zona de desborde, esta puede ser un vector, una lista, etc. Osea que si se busca el elemento y no se lo encuentra en la posicion, se va a la zona de desborde a buscarlo linealmente.
+
+DIAGRAMA DE FUNCION HASH, TABLA DE HASH y COLISIONES
+<div align="center">
+<img width="70%" src="img/diagrama_tabla_hash.png">
+</div>
+
+
+
--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -1,17 +1,244 @@
 #include "pa2m.h"
+#include "src/hash.h"
+#include "src/hash_estructura_privada.h"
+#include <stdlib.h>
 
-void prueba_simple()
+size_t funcion_hash1(const char *clave)
 {
-	int i = 14;
-	int j = 16;
-	pa2m_afirmar(i != j, "i=14 es diferente de j=16");
+	size_t numero = 5381;
+	int entero;
+
+	while ((entero = *clave++)) {
+		numero = (((numero << 5) + numero) + (size_t)entero);
+	}
+
+	return numero;
+}
+
+bool funcion_bool(const char *clave, void *valor, void *aux)
+{
+	if (!aux)
+		return true;
+
+	return *((int *)valor) < *((int *)aux);
+}
+
+void pruebas()
+{
+	pa2m_nuevo_grupo(
+		"\n======================== CREAR HASH ========================");
+	hash_t *hash_capacidad0 = hash_crear(0);
+	hash_t *hash = hash_crear(10);
+	pa2m_afirmar(
+		hash && hash->capacidad == 10,
+		"Se prueba crear el hash con una capacidad inicial mayor a 3");
+	pa2m_afirmar(
+		hash_capacidad0 && hash_capacidad0->capacidad == 3,
+		"Se prueba crear el hash con una capacidad inicial menor a 3");
+	hash_destruir(hash_capacidad0);
+
+	pa2m_nuevo_grupo(
+		"\n======================== INSERTAR ========================");
+	void *elemento = (void *)1;
+	void *anterior = NULL;
+	hash_t *hash_nulo = hash_insertar(NULL, "clave1", elemento, &anterior);
+	pa2m_afirmar(!hash_nulo, "Insertar en hash nulo devuelve NULL");
+	hash_t *hash_clave_nula =
+		hash_insertar(hash, NULL, elemento, &anterior);
+	pa2m_afirmar(!hash_clave_nula, "Insertar con clave nula devuelve NULL");
+	hash_destruir(hash_nulo);
+	hash_destruir(hash_clave_nula);
+
+	hash = hash_insertar(hash, "clave0", NULL, &anterior);
+	pa2m_afirmar(
+		hash_contiene(hash, "clave0") && hash->cantidad == 1,
+		"Insertar con clave valida y elemento nulo inserta correctamente");
+	hash = hash_insertar(hash, "clave1", elemento, &anterior);
+	pa2m_afirmar(
+		hash_contiene(hash, "clave1") && hash->cantidad == 2,
+		"Insertar con clave valida y elemento valido inserta correctamente");
+
+	void *elemento2 = (void *)2;
+	hash = hash_insertar(hash, "clave2", elemento2, &anterior);
+	void *elemento3 = (void *)3;
+	hash = hash_insertar(hash, "clave3", elemento3, &anterior);
+	void *elemento4 = (void *)4;
+	hash = hash_insertar(hash, "clave4", elemento4, &anterior);
+	void *elemento5 = (void *)5;
+	hash = hash_insertar(hash, "clave5", elemento5, &anterior);
+	void *elemento6 = (void *)6;
+	hash = hash_insertar(hash, "clave6", elemento6, &anterior);
+	pa2m_afirmar(hash_contiene(hash, "clave2") &&
+			     hash_contiene(hash, "clave3") &&
+			     hash_contiene(hash, "clave4") &&
+			     hash_contiene(hash, "clave5") &&
+			     hash_contiene(hash, "clave6") &&
+			     hash->cantidad == 7,
+		     "se prueban insertar varios elementos");
+
+	void *elemento7 = (void *)7;
+	hash = hash_insertar(hash, "clave7", elemento7, &anterior);
+	void *elemento8 = (void *)8;
+	hash = hash_insertar(hash, "clave8", elemento8, &anterior);
+	pa2m_afirmar(
+		hash_contiene(hash, "clave8") && hash->cantidad == 9 &&
+			hash->capacidad == 20,
+		"luego de insertar varios elementos, se rehashea correctamente");
+
+	void *elemento_actualizado = (void *)100;
+	hash_insertar(hash, "clave8", elemento_actualizado, &anterior);
+	pa2m_afirmar(hash_obtener(hash, "clave8") == elemento_actualizado &&
+			     anterior == elemento8,
+		     "se prueba actualizar un elemento");
+
+	pa2m_nuevo_grupo(
+		"\n======================== ELIMINAR ========================");
+	void *elemento_eliminado = hash_quitar(NULL, "clave11");
+	pa2m_afirmar(!elemento_eliminado,
+		     "Eliminar en hash nulo devuelve NULL");
+	elemento_eliminado = hash_quitar(hash, NULL);
+	pa2m_afirmar(!elemento_eliminado,
+		     "intentar eliminar una clave nula devuelve NULL");
+	elemento_eliminado = hash_quitar(hash, "aaaaaaaaaaa");
+	pa2m_afirmar(
+		!elemento_eliminado,
+		"intentar eliminar un elemento con clave inexistente devuelve NULL");
+	elemento_eliminado = hash_quitar(hash, "clave2");
+	pa2m_afirmar(
+		elemento_eliminado == elemento2 && hash->cantidad == 8,
+		"se puede eliminar un elemento y la cantidad de elementos disminuye");
+	elemento_eliminado = hash_quitar(hash, "clave2");
+	pa2m_afirmar(
+		elemento_eliminado == NULL,
+		"si se intenta eliminar un elemento ya eliminado, devuelve NULL");
+
+	pa2m_nuevo_grupo(
+		"\n======================== OBTENER ========================");
+	void *elemento_obtenido = hash_obtener(NULL, "clave11");
+	pa2m_afirmar(!elemento_obtenido, "obtener en hash nulo devuelve NULL");
+	elemento_obtenido = hash_obtener(hash, NULL);
+	pa2m_afirmar(!elemento_obtenido,
+		     "intentar obtener una clave nula devuelve NULL");
+	elemento_obtenido = hash_obtener(hash, "aaaaaaaaaaa");
+	pa2m_afirmar(
+		!elemento_obtenido,
+		"intentar obtener un elemento con clave inexistente devuelve NULL");
+	elemento_obtenido = hash_obtener(hash, "clave4");
+	pa2m_afirmar(elemento_obtenido == elemento4,
+		     "se puede obtener un elementoe");
+	elemento_obtenido = hash_obtener(hash, "clave4");
+	pa2m_afirmar(elemento_obtenido == elemento4,
+		     "si se intenta obtener un elemento ya obtenido");
+
+	pa2m_nuevo_grupo(
+		"\n======================== CONTENER ========================");
+	pa2m_afirmar(!hash_obtener(hash, "clave11"),
+		     "obtener en hash nulo devuelve NULL");
+	pa2m_afirmar(!hash_obtener(hash, NULL),
+		     "intentar obtener una clave nula devuelve NULL");
+	pa2m_afirmar(
+		!hash_obtener(hash, "aaaaaaaaaaa"),
+		"intentar obtener un elemento con clave inexistente devuelve NULL");
+	pa2m_afirmar(hash_obtener(hash, "clave4") == elemento4,
+		     "se puede obtener un elemento");
+
+	pa2m_nuevo_grupo(
+		"\n======================== CANTIDAD ========================");
+	pa2m_afirmar(hash_cantidad(NULL) == 0,
+		     "la cantidad de elementos de un hash nulo es 0");
+	hash_t *hash_vacio = hash_crear(0);
+	pa2m_afirmar(hash_cantidad(hash_vacio) == 0,
+		     "la cantidad de elementos de un hash vacio es 0");
+	pa2m_afirmar(hash_cantidad(hash) == 8,
+		     "la cantidad de elementos de un hash es correcta");
+	hash_destruir(hash_vacio);
+	hash_destruir(hash);
+
+	pa2m_nuevo_grupo(
+		"\n======================== ITERADOR INTERNO ========================");
+	hash = hash_crear(3);
+
+	int valor1 = 1;
+	int valor2 = 2;
+	int valor3 = 3;
+
+	hash = hash_insertar(hash, "1", &valor1, NULL);
+	hash = hash_insertar(hash, "2", &valor2, NULL);
+	hash = hash_insertar(hash, "3", &valor3, NULL);
+	pa2m_afirmar(hash_con_cada_clave(NULL, funcion_bool, elemento2) == 0,
+		     "iterar en un hash nulo devuelve 0");
+	pa2m_afirmar(
+		hash_con_cada_clave(hash, NULL, elemento2) == 0,
+		"iterar en un hash valido con una funcion nula devuelve 0");
+	pa2m_afirmar(
+		hash_con_cada_clave(hash, NULL, elemento2) == 0,
+		"iterar en un hash valido con una funcion nula devuelve 0");
+	pa2m_afirmar(
+		hash_con_cada_clave(hash, funcion_bool, NULL) == 3,
+		"iterar en un hash valido con una funcion valida y aux nulo funciona");
+	int aux = 10;
+	pa2m_afirmar(hash_con_cada_clave(hash, funcion_bool, &aux) == 3,
+		     "se puede iterar el hash en su totalidad");
+	pa2m_afirmar(
+		hash_con_cada_clave(hash, funcion_bool, &elemento2) == 1,
+		"se puede iterar el hash y que se detenga cuando la funcion devuelve false");
+
+	hash_destruir(hash);
+}
+
+void prueba_funcion_hash()
+{
+	hash_t *hash = hash_crear(100000);
+	pa2m_nuevo_grupo(
+		"\n======================== PRUEBA FUNCION HASH ========================");
+	size_t posicion_hash = funcion_hash1("clave1") % hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash = funcion_hash1("ggrebdfbfhssrt") % hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash = funcion_hash1("dsdeaf") % hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash = funcion_hash1("aadsfdsg") % hash->capacidad;
+	printf("posicion hash: %li\n", posicion_hash);
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash = funcion_hash1("bfnghgfh") % hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash = funcion_hash1("cngfjfhd") % hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash = funcion_hash1("fdgdafgser") % hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash = funcion_hash1("fbbcxbb") % hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash =
+		funcion_hash1(
+			"rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr") %
+		hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash = funcion_hash1("1") % hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash = funcion_hash1("ysyaaaaa") % hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
+	posicion_hash =
+		funcion_hash1("_______________________________________") %
+		hash->capacidad;
+	pa2m_afirmar(posicion_hash < hash->capacidad,
+		     "valor correcto funcion hash");
 }
 
 int main()
 {
-	pa2m_nuevo_grupo(
-		"\n======================== XXX ========================");
-	prueba_simple();
+	pruebas();
+	//prueba_funcion_hash();
 
 	return pa2m_mostrar_reporte();
 }
